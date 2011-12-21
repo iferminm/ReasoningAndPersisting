700382f23130edbb05540f757fa6d0701f00dd8b
Israel Fermin Montilla <iferminm@gmail.com>
2011-12-21 17:47:39 -0430

    added a method to import the reasoned graph into Virtuoso

--- src/com/main/VirtuosoImporter.java
+++ src/com/main/VirtuosoImporter.java
@@ -1,5 +1,22 @@
 package com.main;
 
+import java.io.FileInputStream;
+
+import virtuoso.jena.driver.VirtModel;
+import com.hp.hpl.jena.rdf.model.Model;
+
 public class VirtuosoImporter {
 
+	private String connection = "jdbc:virtuoso://localhost:1111";
+	private String user = "dba";
+	private String pwd = "virtuoso";
+	
+	public void toVirtuoso(String file) {
+		try {
+			Model virtModel = VirtModel.openDatabaseModel("thesis:test", this.connection, this.user, this.pwd);
+			virtModel.read(new FileInputStream(file), "http://localhost/thesis/virt");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
