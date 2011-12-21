1320d09f59041dff27c6154cb41b8eed8d4ea3cb
Israel Fermin Montilla <iferminm@gmail.com>
2011-12-21 15:47:04 -0430

    Some main program to test the OwlReasoner class

--- src/com/main/Main.java
+++ src/com/main/Main.java
@@ -1,9 +1,14 @@
 package com.main;
 
 public class Main {
+
+	private static String model = "/var/www/ontologies/ThesisOntology.owl";
+	private static String reasoned = "/var/www/ontologies/reasoned.owl";
 	
 	public static void main(String args[]) {
-		
+		OwlReasoner or = new OwlReasoner(model, reasoned);
+		or.generateReasonedModel();
+		System.out.println("Done.");
 	}
 
 }
